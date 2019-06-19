2d65bfbc4fc158a8bac9c051d41443c07a5ceb7a
Tommi Rautanen <tommi.rautanen@cybercom.com>
2019-06-19 17:58:37 +0300

    Use 10 years as threshold when preventing live stream jumping to start instead of 1 year.

--- src/streaming/controllers/PlaybackController.js
+++ src/streaming/controllers/PlaybackController.js
@@ -399,11 +399,11 @@ function PlaybackController() {
         if (!DVRWindow) return NaN;
         if (currentTime > DVRWindow.end) {
             actualTime = Math.max(DVRWindow.end - streamInfo.manifestInfo.minBufferTime * 2, DVRWindow.start);
-        } else if (currentTime + 0.250 < DVRWindow.start && DVRWindow.start - currentTime > DVRWindow.start - 31536000) {
+        } else if (currentTime + 0.250 < DVRWindow.start && DVRWindow.start - currentTime > DVRWindow.start - 315360000) {
             // Checking currentTime plus 250ms as the 'timeupdate' is fired with a frequency between 4Hz and 66Hz
             // https://developer.mozilla.org/en-US/docs/Web/Events/timeupdate
             // http://w3c.github.io/html/single-page.html#offsets-into-the-media-resource
-	    // Making also sure that difference between DVRWindow.start and currentTime reported by video element isn't bigger than 1y
+	    // Making also sure that difference between DVRWindow.start and currentTime reported by video element isn't bigger than 10 years
 	    // https://github.com/Dash-Industry-Forum/dash.js/issues/2993
             actualTime = DVRWindow.start;
         } else {
